7737b0a6f57251c5c985fab23f44cd361a72994a
ReineDeLaNight <germain40k@hotmail.fr>
2020-11-29 12:57:02 +0100

    ajout admin

--- router.js
+++ router.js
@@ -1,4 +1,30 @@
 const router = require('express').Router()
+const fs = require('fs')
+const multer = require("multer")
+
+const DIR = './texts'
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, DIR);
+    },
+    filename: (req, file, cb) => {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-');
+        cb(null, fileName)
+    }
+});
+
+var upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "application/json" || file.mimetype == "text/plain") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
+});
 
 router.get('/text/:text', async (req, res) => {
     const text = req.params.text
@@ -9,6 +35,29 @@ router.get('/text/:text', async (req, res) => {
         res.send(err).status(404)
     }
 
+})
+router.get('/textListe', async (req, res) => {
+    try {
+        var files = fs.readdirSync('texts');
+        res.send(files).status(200)
+    } catch (err) {
+        res.send(err).status(404)
+    }
+})
+
+router.post('/multiple-files', upload.array('files', 10), (req, res) => {
+    res.send('file sent').status(200)
+})
+
+router.delete('/delete-file/:file', (req, res) => {
+    console.log("delete")
+    const file = req.params.file
+    console.log(file)
+    fs.unlink('texts/' + file, function (err) {
+        if (err) throw err;
+        // if no error, file has been deleted successfully
+        res.send("file deleted").status(200)
+    });  
 })
 
 
